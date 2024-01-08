af80f97039903f0b055f6e353d1d869d0ded489c
DeepSh4d0w <claudio.roberto.santana.f@gmail.com>
2024-01-08 15:29:40 +0000

    feat: add `database.js` and `.env`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,3 +1,8 @@
-export default function Status(request, response) {
+import database from "../../../../infra/database.js";
+
+export default async function Status(request, response) {
+  const result = await database.query("SELECT 1+1 as sum;");
+  console.log(result.rows);
+
   response.status(200).json({ chave: "valor" });
 }
