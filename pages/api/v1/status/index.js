70eb4123d0256a4c99f0ffdb6ed3330481bdf683
DeepSh4d0w <claudio.roberto.santana.f@gmail.com>
2024-01-09 00:11:52 +0000

    feat: finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,29 @@
 import database from "infra/database.js";
 
 export default async function Status(request, response) {
-  const result = await database.query("SELECT 1+1 as sum;");
-  console.log(result.rows);
+  const updatedAt = new Date().toISOString();
 
-  response.status(200).json({ chave: "valor" });
+  const rawVersion = await database.query("SHOW server_version;");
+  const version = rawVersion.rows[0].server_version;
+
+  const rawMaxConnections = await database.query("SHOW max_connections;");
+  const maxConnections = rawMaxConnections.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const rawOpenedConnections = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname=$1;",
+    values: [databaseName],
+  });
+  const OpenedConnections = rawOpenedConnections.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependences: {
+      database: {
+        max_connections: parseInt(maxConnections),
+        opened_connections: OpenedConnections,
+        version: version,
+      },
+    },
+  });
 }
--- test/integration/api/v1/status/get.spec.js
+++ test/integration/api/v1/status/get.spec.js
@@ -1,7 +1,31 @@
-describe("- GET `/api/v1/status`", () => {
-  it("should be return status 200", async () => {
-    const response = await fetch("http://localhost:3000/api/v1/status");
+describe("○ GET `/api/v1/status`", () => {
+  let response, responseBody;
+
+  it("should be status 200", async () => {
+    response = await fetch("http://localhost:3000/api/v1/status");
+    responseBody = await response.json();
 
     expect(response.status).toBe(200);
+  });
+
+  it("should be `updated_at` formated as ISO 8601", () => {
+    const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+    expect(responseBody.updated_at).toBe(parsedUpdatedAt);
+  });
+
+  describe("Dependences", () => {
+    describe("Database", () => {
+      it("should be `max_connection` as 100", () => {
+        expect(responseBody.dependences.database.max_connections).toBe(100);
+      });
+
+      it("should be `opened_connections` as 1", () => {
+        expect(responseBody.dependences.database.opened_connections).toBe(1);
+      });
+
+      it("should be `version` as '16.0'", () => {
+        expect(responseBody.dependences.database.version).toBe("16.0");
+      });
+    });
   });
 });
